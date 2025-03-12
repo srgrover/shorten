8c4b1f3e6ffbe6731d9abe9af9f6e12bc96e60ad
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-03-12 19:37:44 +0000

    Create top menu and added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TopMenu } from "@/components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <TopMenu />
+        { children }
       </body>
     </html>
   );
