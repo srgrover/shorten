cbd614bb8cfb4d90bf19d90898a1a040092e84c5
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-03-12 19:39:29 +0000

    Created settings page

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Settings - Dashboard",
+};
+
+export default function SettingsPage() {
+    return (
+        <h1>Dashboard page 
+
+            settings
+        </h1>
+        
+    );
+}
