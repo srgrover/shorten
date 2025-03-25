3cb453ae97305640215c26a5e3fe072fe66e732e
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-03-25 13:21:49 +0000

    Change prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-const prisma = global.prisma || new PrismaClient({
-  log: ['error']
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
