203fe8f8ca0a22fbeeea859e4bc2139acf76a778
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-03-25 13:01:11 +0000

    Login page completed

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "./ui/LoginForm"
+
+export default function Login() {
+    return (
+        <div className="flex min-w-screen flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <LoginForm />
+        </div>
+    )
+}
