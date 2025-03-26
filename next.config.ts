da840cd76384ce6ea2a4e556c7ebd5125eeb0e85
Jonathan Moya <djgrovezone@gmail.com>
2025-03-26 13:47:57 +0100

    Add Flowbite React plugin and configure remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      },
+    ]
+  }
 };
 
-export default nextConfig;
+export default withFlowbiteReact(nextConfig);
