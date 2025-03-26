84242d10d90cb82ea82bf578c4e941ee20ae7715
Jonathan Moya <djgrovezone@gmail.com>
2025-03-26 14:04:13 +0100

    Implement session check and redirect in Login component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from "@/auth.config"
 import { LoginForm } from "./ui/LoginForm"
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+    const session = await auth();
+    if(session) return redirect('/dashboard');
+    
     return (
         <div className="flex min-w-screen flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <LoginForm />
