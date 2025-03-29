2dee555f4a945bb9b5c9b075be45aaabd5ce8b42
Jonathan Moya <djgrovezone@gmail.com>
2025-03-29 10:47:54 +0100

    The button padding and width were adjusted to improve visual alignment and responsiveness. The changes ensure a more consistent user experience across different screen sizes.

--- src/app/auth/login/ui/ButtonProvider.tsx
+++ src/app/auth/login/ui/ButtonProvider.tsx
@@ -12,7 +12,7 @@ export const ButtonProvider = ({ provider, icon }: Props) => {
         <button
             onClick={async() => {await login(provider)} }
             type="button"
-            className="px-3 py-2 flex items-center gap-2 justify-center shadow-sm shadow-gray-400/50 rounded-md cursor-pointer hover:bg-gray-100">
+            className="px-3 py-1 flex items-center min-w-2/3 w-100 gap-2 justify-center shadow-sm shadow-gray-400/50 rounded-md cursor-pointer hover:bg-gray-100">
             {icon}
             Signin with {provider.toWellFormed()}
         </button>
