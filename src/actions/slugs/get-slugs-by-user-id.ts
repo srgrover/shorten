291d71b6b34d9346d7ff13792e6a98cecb302724
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-10-13 22:21:48 +0000

    New action for get slug from db by user id

---
+++ src/actions/slugs/get-slugs-by-user-id.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { auth } from "@/auth.config";
+import prisma from "@/lib/prisma";
+
+export const getSlugsByUserId = async () => {
+  const session = await auth();
+
+  if (!session?.user) {
+    return {
+      ok: false,
+      message: "Debe iniciar sesión para poder ver sus órdenes",
+    };
+  }
+
+  const slugs = await prisma.slug.findMany({
+    where: {
+      userId: session.user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    // include: {
+    //   user: true,
+    // },
+
+  });
+
+  return {
+    ok: true,
+    slugs: slugs || [],
+  }
+}
