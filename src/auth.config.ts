9f517194495cc22de3b019568e05860f26e4d238
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-10-13 22:20:29 +0000

    Improve auth js

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,30 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, GitHub],
-})
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+
+const config: NextAuthConfig = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Permite redirecciones relativas
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+
+      // Permite redirecciones a otros dominios si vienen en la URL
+      if (new URL(url).origin === baseUrl) return url;
+      
+      // Si no es ninguno de los anteriores (ej. primer login), redirige a dashboard
+      return baseUrl + '/dashboard';
+    },
+  }
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
