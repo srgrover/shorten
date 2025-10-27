8ab08fe4d63d91235e3915e8f5a8ec97caef70fb
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-10-27 23:19:06 +0000

    refactor

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
 
-export default config;
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {}
+  }
+}
+
+export default config
