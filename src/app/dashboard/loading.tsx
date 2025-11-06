b01e0ba329ff70c65fa9ad267994271ec360b71c
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2025-11-06 16:31:26 +0000

    Loading page changed

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -2,6 +2,9 @@ import { LuLoader } from "react-icons/lu";
 
 export default function Home() {
   return (
-    <LuLoader size={15} className="animate-spin" />
+    <div className="flex justify-center items-center flex-col">
+      <LuLoader size={15} className="animate-spin" />
+      Loading slugs... Please wait
+    </div>
   );
 }
